19adcc692523debdc6c76b5fc1b58ae0d533fb8e
Shrutir1810 <shrutir1810@gmail.com>
2024-05-20 16:39:42 +0530

    Updated app.js

--- Chat/app.js
+++ Chat/app.js
@@ -1,28 +1,23 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const open = require('open'); // Add this line
-const usernames = [];
+var express = require("express"),
+  app = express(),
+  server = require("http").createServer(app),
+  io = require("socket.io")(server); // Update this line
+usernames = [];
+server.listen(process.env.PORT || 3000);
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Server listening on port 3000');
-  open('http://localhost:3000'); // Add this line to open the browser automatically
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+  //res.sendFile(__dirname + '/style.css');
 });
 
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js', express.static(__dirname + '/js'));
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){ // Update this line
-  socket.on('new user', function(data, callback){
-    if(usernames.indexOf(data) !== -1){
+io.sockets.on("connection", function (socket) {
+  socket.on("new user", function (data, callback) {
+    if (usernames.indexOf(data) != -1) {
       callback(false);
-    }
-    else{
+    } else {
       callback(true);
       socket.username = data;
       usernames.push(socket.username);
@@ -30,17 +25,17 @@ io.on('connection', function(socket){ // Update this line
     }
   });
 
-  function updateUsernames(){
-    io.emit('usernames', usernames); // Update this line
+  function updateUsernames() {
+    io.sockets.emit("usernames", usernames);
   }
 
-  socket.on('send message', function(data){
-    io.emit('new message', {msg: data, user: socket.username}); // Update this line
+  socket.on("send message", function (data) {
+    io.sockets.emit("new message", { msg: data, user: socket.username });
   });
 
-  socket.on('disconnect', function(data){
-    if(!socket.username) return;
-    usernames.splice(usernames.indexOf(socket.username), 1);
+  socket.on("disconnect", function (data) {
+    if (!socket.username) return;
+    usernames.splice(usernames.indexOf(socket.username, 1));
     updateUsernames();
   });
 });
